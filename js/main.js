a3de8c92d1f2112146e626aa3c3760533716c0c8
milan978gh <milan978@gmail.com>
2020-10-30 18:34:40 +0100

    update

--- js/main.js
+++ js/main.js
@@ -82,5 +82,53 @@ const menu = [
 	},
 ];
 
+// ---------------------------------------------------
+// simple menu
+/* const sectionCenter = document.querySelector('.section-center');
+
+window.addEventListener('DOMContentLoaded', function () {
+	let displayMenu = menu.map(function (item) {
+		return `<article class="menu-item">
+		<img src=${item.img} alt=${item.title} class="photo">
+		<div class="item-info">
+			<header>
+				<h4>${item.title}</h4>
+				<h4 class="price">$${item.price}</h4>
+			</header>
+			<p class="item-text">
+				${item.desc}
+			</p>
+		</div>
+	</article>`;
+	});
+	displayMenu = displayMenu.join('');
+	sectionCenter.innerHTML = displayMenu;
+}); */
+// ---------------------------------------------------------
+
+const sectionCenter = document.querySelector('.section-center');
 
 
+window.addEventListener('DOMContentLoaded', function () {
+	displayMenuItems(menu);
+});
+
+
+function displayMenuItems(menuItems) {
+	let displayMenu = menuItems.map(function (item) {
+		return `<article class="menu-item">
+					<img src=${item.img} alt=${item.title} class="photo">
+					<div class="item-info">
+						<header>
+							<h4>${item.title}</h4>
+							<h4 class="price">$${item.price}</h4>
+						</header>
+						<p class="item-text">
+							${item.desc}
+						</p>
+					</div>
+				</article>`;
+	});
+	displayMenu = displayMenu.join('');
+	sectionCenter.innerHTML = displayMenu;
+}
